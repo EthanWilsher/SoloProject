bf72db66de7e12e27f258477287d09cfc29e0ed3
wilshdog <31513793+wilshdog@users.noreply.github.com>
2017-09-07 08:45:46 -0600

    Made an app by myself and added gitignore

---
+++ src/solo/holder/SoloController.java
@@ -0,0 +1,10 @@
+package solo.holder;
+
+public class SoloController
+{
+	public void start()
+	{
+		System.out.println("YO! look at me makin Programs!");
+		System.out.println("WUBBALUBBADUBDUUUUUB!!");
+	}
+}
---
+++ src/solo/holder/SoloRunner.java
@@ -0,0 +1,10 @@
+package solo.holder;
+
+public class SoloRunner
+{
+	public static void main (String [] args)
+	{
+		SoloController myApp = new SoloController();
+		myApp.start();
+	}
+}
